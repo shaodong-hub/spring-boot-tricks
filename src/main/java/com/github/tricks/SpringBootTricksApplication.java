558af28bd9ab7ad74ada76b3aec3bdfa0d853da9
shaopro <shaopro@qq.com>
2020-09-07 23:31:21 +0800

    写入PID

--- src/main/java/com/github/tricks/SpringBootTricksApplication.java
+++ src/main/java/com/github/tricks/SpringBootTricksApplication.java
@@ -2,12 +2,16 @@ package com.github.tricks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SpringBootTricksApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootTricksApplication.class, args);
+//        SpringApplication.run(SpringBootTricksApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(SpringBootTricksApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 
 }
