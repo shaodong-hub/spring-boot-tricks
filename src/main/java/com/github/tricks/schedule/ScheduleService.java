418518a21d0b13a087201087d2f816234bef93f1
shaopro <shaopro@qq.com>
2020-09-08 12:51:10 +0800

    初始化项目

--- src/main/java/com/github/tricks/schedule/ScheduleService.java
+++ src/main/java/com/github/tricks/schedule/ScheduleService.java
@@ -1,8 +1,6 @@
 package com.github.tricks.schedule;
 
 import lombok.extern.slf4j.Slf4j;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,22 +13,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleService {
 
-    @Scheduled(fixedDelay = 5000)
-    @SchedulerLock(name = "myTask1")
-    public void task1() {
-        log.info("myTask1 - {}", Thread.currentThread().getName());
-    }
 
-    @Scheduled(fixedDelay = 5000)
-    @SchedulerLock(name = "myTask2")
-    public void task2() {
-        log.info("myTask2 - {}", Thread.currentThread().getName());
-    }
-
-    @Scheduled(fixedDelay = 5000)
-    @SchedulerLock(name = "myTask3")
-    public void task3() {
-        log.info("myTask3 - {}", Thread.currentThread().getName());
-    }
 
 }
