95b42270b6d938b278531eff41bcaa5903bb3fb8
shaopro <shaopro@qq.com>
2020-09-14 20:44:00 +0800

    Springboot JIB 打包 docker 镜像

--- src/main/java/com/github/tricks/SpringBootTricksApplication.java
+++ src/main/java/com/github/tricks/SpringBootTricksApplication.java
@@ -1,10 +1,11 @@
 package com.github.tricks;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 @EnableScheduling
 @SpringBootApplication
 public class SpringBootTricksApplication {
--- src/main/java/com/github/tricks/schedule/ScheduleService.java
+++ src/main/java/com/github/tricks/schedule/ScheduleService.java
@@ -5,6 +5,8 @@ import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author 石少东
  * @date 2020-09-08 09:30
@@ -15,21 +17,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class ScheduleService {
 
+    private static final String LOCK_TIME = "100s";
+
     @Scheduled(fixedDelay = 5000)
-    @SchedulerLock(name = "myTask1")
-    public void task1() {
+    @SchedulerLock(name = "myTask1", lockAtMostFor = LOCK_TIME, lockAtLeastFor = LOCK_TIME)
+    public void task1() throws Exception {
+        TimeUnit.SECONDS.sleep(50);
         log.info("myTask1 - {}", Thread.currentThread().getName());
     }
 
     @Scheduled(fixedDelay = 5000)
-    @SchedulerLock(name = "myTask2")
-    public void task2() {
+    @SchedulerLock(name = "myTask2", lockAtMostFor = LOCK_TIME, lockAtLeastFor = LOCK_TIME)
+    public void task2() throws Exception {
+        TimeUnit.SECONDS.sleep(50);
         log.info("myTask2 - {}", Thread.currentThread().getName());
     }
 
     @Scheduled(fixedDelay = 5000)
-    @SchedulerLock(name = "myTask3")
-    public void task3() {
+    @SchedulerLock(name = "myTask3", lockAtMostFor = LOCK_TIME, lockAtLeastFor = LOCK_TIME)
+    public void task3() throws Exception {
+        TimeUnit.SECONDS.sleep(50);
         log.info("myTask3 - {}", Thread.currentThread().getName());
     }
 
