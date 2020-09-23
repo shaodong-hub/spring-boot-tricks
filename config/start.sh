130e705028890271eb6b77983c3bdfe7a800459c
shaopro <shaopro@qq.com>
2020-09-23 18:38:07 +0800

    优化启动脚本

--- config/start.sh
+++ config/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # --------------------------------------------------------------------------
-# 基本的配置
 # The name of the application
 app_name="tricks"
 # 当前工程的路径
@@ -14,21 +13,26 @@ echo "current data: ${format_date}"
 # 代码自检
 bak_folder="/apprun/bak"
 if [ ! -d "$bak_folder" ]; then
-  echo "备份文件不存在，创建 $bak_folder...... "
+  echo "bak folder not found, create $bak_folder...... "
   mkdir "$bak_folder"
   chown -R apprun:apprun "$bak_folder"
 fi
 # 代码自检
 if [ ! -d "${apprun_path}" ]; then
-  echo "${apprun_path} 目录不存在, 我猜是第一次部署 ...... "
+  echo "${apprun_path} not found, I guess it's the first deployment ...... "
 fi
 # --------------------------------------------------------------------------
 # 检查 /etc/systemd/system 目录下是否有 .service 文件, 没有就新建并赋予权限
 # 系统服务基础路径
 base_system_path="/etc/systemd/system/${app_name}.service"
 echo "systemd: ${base_system_path}"
-sudo cp "${new_project_path}"/${app_name}.service ${base_system_path}
-chmod 755 ${base_system_path}
+if [ ! -f "${base_system_path}" ]; then
+  echo "${base_system_path} not found, I guess it's the first deployment ...... "
+  echo "move ${new_project_path}/${app_name}.service to ${base_system_path}"
+  echo "chmod 755 ${base_system_path}"
+#  sudo cp "${new_project_path}"/${app_name}.service ${base_system_path}
+#  chmod 755 ${base_system_path}
+fi
 # --------------------------------------------------------------------------
 # 重命名管理平台并赋权限
 if [ $# == 0 ]; then
